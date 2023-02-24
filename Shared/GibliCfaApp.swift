6dc20ae141f8a51a7e476fde41bb106d5d36e6e5
sebastienLidem <sebastien@lidem.eu>
2023-02-24 11:27:39 +0100

    Initial Commit

---
+++ Shared/GibliCfaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GibliCfaApp.swift
+//  Shared
+//
+//  Created by stage on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct GibliCfaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
